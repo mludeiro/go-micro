32a4970e5c746481ffb607f4f38eb24579438afc
mludeiro <mludeiro@github.com>
2021-11-16 22:57:33 -0300

    Improved db logger

--- repository/database.go
+++ repository/database.go
@@ -1,17 +1,32 @@
 package repository
 
 import (
+	"go-micro/tools"
 	"log"
+	"time"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 var db *gorm.DB
 
 func Initialize() {
+	newLogger := logger.New(
+		tools.GetLogger(), // io writer
+		logger.Config{
+			SlowThreshold:             time.Second, // Slow SQL threshold
+			LogLevel:                  logger.Info, // Log level
+			IgnoreRecordNotFoundError: true,        // Ignore ErrRecordNotFound error for logger
+			Colorful:                  true,        // Disable color
+		},
+	)
+
 	dsn := "host=localhost user=postgres password=postgres dbname=go_micro port=5432 sslmode=disable TimeZone=Asia/Shanghai"
-	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: newLogger,
+	})
 
 	if err != nil {
 		log.Fatal("Cannot initialize database")
@@ -30,5 +45,5 @@ func Initialize() {
 }
 
 func getDB() *gorm.DB {
-	return db.Debug()
+	return db
 }
