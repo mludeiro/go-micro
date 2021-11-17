f3c6adcaf191d2f286f183ed1867d1d8e411a9fd
mludeiro <mludeiro@github.com>
2021-11-16 22:45:35 -0300

    Improve routing paths

--- presentation/webRouter.go
+++ presentation/webRouter.go
@@ -12,16 +12,16 @@ func getRouter() *mux.Router {
 	// this is not necessary
 	cont := ArticlesController{}
 
-	sm.Methods(http.MethodGet).Subrouter().Path("/articles/{id:[0-9]+}").Queries("expand", "{expand:[a-zA-Z0-9]+}").HandlerFunc(cont.GetArticle)
-	sm.Methods(http.MethodGet).Subrouter().Path("/articles/{id:[0-9]+}").HandlerFunc(cont.GetArticle)
-	sm.Methods(http.MethodGet).Subrouter().Path("/articles").Queries("expand", "{expand}").HandlerFunc(cont.GetArticles)
+	sm.Methods(http.MethodGet).Path("/articles/{id:[0-9]+}").Queries("expand", "{expand:[a-zA-Z0-9]+}").HandlerFunc(cont.GetArticle)
+	sm.Methods(http.MethodGet).Path("/articles/{id:[0-9]+}").HandlerFunc(cont.GetArticle)
+	sm.Methods(http.MethodGet).Path("/articles").Queries("expand", "{expand}").HandlerFunc(cont.GetArticles)
 
-	sm.Methods(http.MethodDelete).Subrouter().HandleFunc("/articles/{id:[0-9]+}", cont.DeleteArticle)
-	sm.Methods(http.MethodPost).Subrouter().HandleFunc("/articles", cont.PostArticle)
+	sm.Methods(http.MethodDelete).Path("/articles/{id:[0-9]+}").HandlerFunc(cont.DeleteArticle)
+	sm.Methods(http.MethodPost).Path("/articles").HandlerFunc(cont.PostArticle)
 
 	// Sometimes we only need handlers ;-)
-	sm.Methods(http.MethodGet).Subrouter().Path("/articleTypes").Queries("expand", "{expand:[a-zA-Z0-9]+}").HandlerFunc(GetArticleTypesHandler)
-	sm.Methods(http.MethodGet).Subrouter().HandleFunc("/articleTypes", GetArticleTypesHandler)
+	sm.Methods(http.MethodGet).Path("/articleTypes").Queries("expand", "{expand:[a-zA-Z0-9]+}").HandlerFunc(GetArticleTypesHandler)
+	sm.Methods(http.MethodGet).Path("/articleTypes").HandlerFunc(GetArticleTypesHandler)
 
 	sm.Use(LogMiddleWare)
 
